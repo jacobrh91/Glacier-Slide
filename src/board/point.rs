b852d9bd7e414345bd6f44c4bb46b04db0ffeef1
Jacob Heldenbrand <jacobrh91@gmail.com>
2025-11-27 14:06:11 -0600

    Simplify point.rs

--- src/board/point.rs
+++ src/board/point.rs
@@ -1,36 +1,15 @@
-use std::fmt::Debug;
-use std::fmt::Display;
-
-use serde::ser::SerializeTuple;
-use serde::Serialize;
-use serde::Serializer;
-
-#[derive(PartialEq, Hash, Eq, Clone, Copy)]
-
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct Point {
     pub col: usize,
     pub row: usize,
 }
 
-impl Display for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{})", self.col, self.row)
-    }
-}
-impl Debug for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{})", self.col, self.row)
-    }
-}
-
-impl Serialize for Point {
+impl serde::Serialize for Point {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: Serializer,
+        S: serde::Serializer,
     {
-        let mut tup = serializer.serialize_tuple(2)?;
-        tup.serialize_element(&self.col)?;
-        tup.serialize_element(&self.row)?;
-        tup.end()
+        // Serialize as [col, row]
+        (self.col, self.row).serialize(serializer)
     }
 }
