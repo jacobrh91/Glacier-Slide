bba16d31adc50c8e23244c751c7b4fabead2440a
Jacob Heldenbrand <jacobrh91@gmail.com>
2025-11-27 14:53:28 -0600

    Refactor game.rs

--- src/game.rs
+++ src/game.rs
@@ -2,8 +2,10 @@ use crate::board::Board;
 use crate::game_state::GameState;
 use crate::renderer::Renderer;
 use crate::system::{clear_terminal, exit_game, respond_to_input};
-use crossterm::event::KeyModifiers;
-use crossterm::event::{Event::Key, KeyCode, KeyEvent, KeyEventKind, KeyEventState};
+
+use crossterm::event::{
+    Event, Event::Key, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers,
+};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 use std::cell::RefCell;
@@ -11,7 +13,7 @@ use std::iter;
 use std::rc::Rc;
 
 pub fn get_introduction_section() -> Vec<String> {
-    let intro = r"Welcome to
+    const INTRO: &str = r"Welcome to
    ______ __              _                _____  __ _      __    
   / ____// /____ _ _____ (_)___   _____   / ___/ / /(_)____/ /___ 
  / / __ / // __ `// ___// // _ \ / ___/   \__ \ / // // __  // _ \
@@ -24,12 +26,12 @@ pub fn get_introduction_section() -> Vec<String> {
   Press 'G' or 'g' to give up and show the solution.
   Press 'Q' or 'Ctrl-C' to exit.
 ";
-    let parts: Vec<String> = intro.split("\n").map(String::from).collect();
-    parts
+
+    INTRO.lines().map(str::to_owned).collect()
 }
 
 fn play_next_input_handler(play_again_signal: &mut bool) {
-    let mut event_handler = |event: crossterm::event::Event| {
+    let mut event_handler = |event: Event| {
         if let Key(KeyEvent {
             code,
             modifiers,
@@ -37,47 +39,45 @@ fn play_next_input_handler(play_again_signal: &mut bool) {
             state: KeyEventState::NONE,
         }) = event
         {
-            match code {
-                KeyCode::Char(c) if c == 'c' && modifiers == KeyModifiers::CONTROL => {
-                    exit_game();
-                }
-                KeyCode::Char('Q') => exit_game(),
-                KeyCode::Char('\u{0020}') => *play_again_signal = true,
-                _ => (),
+            match (code, modifiers) {
+                (KeyCode::Char('c'), m) if m == KeyModifiers::CONTROL => exit_game(),
+                (KeyCode::Char('Q'), _) => exit_game(),
+                (KeyCode::Char(' '), _) => *play_again_signal = true,
+                _ => {}
             }
         }
     };
+
     respond_to_input(&mut event_handler);
 }
 
 pub fn start_game(game_state: GameState) {
-    clear_terminal();
     let game_config = game_state.config.clone();
-
     let rc_game_state = Rc::new(RefCell::new(game_state));
 
     loop {
-        // Disable the raw mode, so that during puzzle generation,
-        // info can be printed to the terminal correctly.
+        // Disable raw mode so puzzle generation and intro text print nicely.
+        let _ = disable_raw_mode();
         clear_terminal();
-        disable_raw_mode().unwrap();
 
-        for i in get_introduction_section() {
-            println!("{}", &i);
+        for line in get_introduction_section() {
+            println!("{line}");
         }
+
         let mut board = Board::generate_solvable_board(&game_config);
 
-        // Now that you have a board, prepare to play it.
-        enable_raw_mode().unwrap();
+        // Now that we have a board, prepare to play it.
+        let _ = enable_raw_mode();
         board.attach_game_state(Rc::clone(&rc_game_state));
 
         play_board(&mut board);
 
-        // After the level has been solved, reset the show solution state.
+        // After the level has been solved, reset the show-solution state.
         rc_game_state.borrow_mut().display_solution = false;
+
         let mut play_next_game = false;
         while !play_next_game {
-            play_next_input_handler(&mut play_next_game)
+            play_next_input_handler(&mut play_next_game);
         }
     }
 }
@@ -91,44 +91,38 @@ fn play_board(board: &mut Board) {
     let interior_mut_board = Rc::new(RefCell::new(board));
 
     let render_callback = || {
-        let mut output = Vec::<String>::new();
-
         let borrowed_board = interior_mut_board.borrow();
         let game_state_opt = borrowed_board.game_state.as_ref();
+
+        let mut output = Vec::<String>::new();
 
         //////////////////
         // Intro section
         //////////////////
-        let intro = get_introduction_section();
-        output.extend(intro);
+        output.extend(get_introduction_section());
 
         /////////////////
         // Solved count
         /////////////////
-        let solved_count: u16 = game_state_opt
-            .map(|x| x.borrow().levels_solved)
+        let solved_count = game_state_opt
+            .map(|state| state.borrow().levels_solved)
             .unwrap_or(0);
-        let boards_solved: String = format!("Levels solved: {}", solved_count);
-
-        output.push(boards_solved);
-        output.push(String::from("\n"));
+        output.push(format!("Levels solved: {}", solved_count));
+        output.push(String::new());
 
         //////////////////
         // Board section
         //////////////////
-        let board_section = borrowed_board.render_board();
-        output.extend(board_section);
+        output.extend(borrowed_board.render_board());
 
-        let mut winning_text = String::from("");
         if borrowed_board.player_won() {
-            winning_text = String::from("You won! Press 'Space' to play again.");
+            output.push("You won! Press 'Space' to play again.".to_string());
         }
-        output.push(winning_text);
-        output.push(String::from("\n"));
+        output.push(String::new());
 
         let (debug, player_gave_up) = game_state_opt
-            .map(|x| {
-                let game_state = x.borrow();
+            .map(|state| {
+                let game_state = state.borrow();
                 (game_state.config.debug, game_state.display_solution)
             })
             .unwrap_or((false, false));
@@ -140,10 +134,9 @@ fn play_board(board: &mut Board) {
             let solution_str = borrowed_board
                 .solution
                 .as_ref()
-                .and_then(|x| x.get_solution_string())
-                .unwrap_or(String::from("Unknown"));
-            let solution: String = format!("Solution: {}", solution_str);
-            output.push(solution);
+                .and_then(|solution| solution.get_solution_string())
+                .unwrap_or_else(|| "Unknown".to_string());
+            output.push(format!("Solution: {}", solution_str));
         }
 
         ////////////////////////////
@@ -151,21 +144,22 @@ fn play_board(board: &mut Board) {
         ////////////////////////////
         if debug {
             let pos = borrowed_board.player.0;
-            let current_position: String = format!("Position: {}, {}", pos.col, pos.row);
-            output.push(current_position);
-            let move_queue: String =
-                format!("Move Queue: {:?}", interior_mut_board.borrow().move_queue);
+            output.push(format!("Position: {}, {}", pos.col, pos.row));
+
+            let move_queue = format!("Move Queue: {:?}", borrowed_board.move_queue);
             output.push(move_queue);
+
             let edges_traversed = borrowed_board
                 .solution
                 .as_ref()
-                .map(|x| x.edges_traversed.to_string())
-                .unwrap_or(String::from("Unknown"));
+                .map(|solution| solution.edges_traversed.to_string())
+                .unwrap_or_else(|| "Unknown".to_string());
             output.push(format!(
                 "Edges traversed to find solution: {}",
                 edges_traversed
-            ))
+            ));
         }
+
         output
     };
 
@@ -174,10 +168,10 @@ fn play_board(board: &mut Board) {
         board_mref.respond_to_input(key_code)
     };
 
-    let player_moves_iterator = iter::from_fn(|| {
-        let board_ref = Rc::clone(&interior_mut_board);
-        let mut board_mref = board_ref.borrow_mut();
-        board_mref.process_move()
+    let board_ref_for_moves = Rc::clone(&interior_mut_board);
+    let player_moves_iterator = iter::from_fn(move || {
+        let mut board_ref = board_ref_for_moves.borrow_mut();
+        board_ref.process_move()
     });
 
     let is_game_over = || interior_mut_board.borrow().player_has_won;
@@ -190,5 +184,5 @@ fn play_board(board: &mut Board) {
         50,
     );
 
-    renderer.render_scene()
+    renderer.render_scene();
 }
