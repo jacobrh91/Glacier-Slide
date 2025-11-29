d36a4573e4ae82bf50ff90dbdd094be2bde99fc5
Jacob Heldenbrand <jacobrh91@gmail.com>
2025-11-29 11:47:28 -0600

    Enable CORS

--- src/web_server/mod.rs
+++ src/web_server/mod.rs
@@ -1,16 +1,22 @@
 mod handlers;
 mod metrics;
 
-use axum::{routing::get, Router};
+use axum::{http::Method, routing::get, Router};
 use std::{error::Error, net::SocketAddr};
+use tower_http::cors::{Any, CorsLayer};
 
 pub async fn start_web_server(bind: String) -> Result<(), Box<dyn Error>> {
     let addr: SocketAddr = bind.parse()?;
 
-    // Router wiring is small enough to live here for now.
+    let cors = CorsLayer::new()
+        .allow_origin(Any) // Do not restrict the origin
+        .allow_methods([Method::GET])
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/health", get(handlers::health_handler))
-        .route("/board", get(handlers::board_handler));
+        .route("/board", get(handlers::board_handler))
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
     println!("listening on http://{}", listener.local_addr()?);
